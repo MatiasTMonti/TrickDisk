96e86c3d57ceeb7c896adb3e6bdb217de5c1c229
Matias <matiasmonti2011@hotmail.com>
2023-11-17 00:24:32 -0300

    Add Script PlayerPrefs Reset

---
+++ TrickDisc/Assets/Scripts/Tools/PlayerPrefsTool.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEditor;
+
+public class PlayerPrefsTool : MonoBehaviour
+{
+    [MenuItem("Tools/Reset PlayerPrefs")]
+    public static void ResetPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+        Debug.Log("PlayerPrefs reset successfully.");
+    }
+
+    [MenuItem("Tools/PlayerPrefs Tool")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(PlayerPrefsTool), false, "PlayerPrefs Tool");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("PlayerPrefs Tool", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Reset PlayerPrefs"))
+        {
+            ResetPlayerPrefs();
+        }
+    }
+}
